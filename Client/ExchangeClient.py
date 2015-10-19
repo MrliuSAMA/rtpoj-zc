ceadc5e4854a5ec225168388480912fc5dece153
MrLiuSAMA <1021237318@qq.com>
2015-10-19 22:20:18 +0800

    客户端的一大波改动，无关逻辑，但是改动了很多的细节

--- Client/ExchangeClient.py
+++ Client/ExchangeClient.py
@@ -1,45 +1,49 @@
+#coding=utf-8
 #!/usr/bin/env python
 import getopt
 import sys
 import subprocess
-import DebugInfo
-import FindCheck
 import pprint
 import re
+import logging
+import os
 
 debug = 0
 version = "0.1"
 
+#	用户参数手册
 def PrintUsage():
 	print "\n\n\t\tCheck RRsets Tools"+"[version %s]\t\t\n" % version 
 	print "usage: chkrr\t[-v version][-f=file_path filemode][-h help manual]"
 	print "\t\t[-D debug info][-d short debug info][-l log record]"
 	print "\t\t[-t=dst_ip query target][-p=dst_port query target]"
-	print "Name *Class Type"
+	print "\t\t[-k=turstkey_path] Name *Class Type"
 	print "\n"
 	print "The most commonly used git commands are:"
 	print "   -v\t Show current version of program"
-	print "   -t\t Decide where the DNS query will be send to"
+	print "   -t\t Decide DNS query Target IP"
+	print "   -p\t Decide DNS query Port"
 	print "   --help\t"
 	print "   -h\t Show the help manual of program"
 	print "   -d\t Show brief debug info to debuger"
 	print "   -D\t Show detailed debug info to debuger"
 	print "   -l\t Recording log to file,log will write each query&answer"
+	print "   -k\t Trusted-key path to verify fetched record"
 	print "   -f\t Batch mode,lunch multi items in file."
 	print "   \t cautions! Batchmode will ignore args except -l"
 	print "\n"
 	print "Some usage tips, maybe it will helps:"
-	print "   1.  chkrr cn. NS -t 127.0.0.1"
-	print "   2.  chkrr cn. IN NS -t 127.0.0.1"
-	print "   3.  chkrr cn. NS -t 127.0.0.1 -d"
-	print "   4.  chkrr cn. NS -t 127.0.0.1 -D"
-	print "   5.  chkrr -f ./queryfile.in"
-	print "   6.  chkrr -f ./queryfile.in -l"
+	print "   1.  ExchangeClient.py cn. NS -t 127.0.0.1"
+	print "   2.  ExchangeClient.py cn. IN NS -t 127.0.0.1"
+	print "   3.  ExchangeClient.py cn. NS -t 127.0.0.1 -d"
+	print "   4.  ExchangeClient.py cn. NS -t 127.0.0.1 -D"
+	print "   5.  ExchangeClient.py -f ./queryfile.in"
 	print "\n"	
 
 
 def PrintVersion():
 	print version
+
 
 
 def VerifyResult(list):
@@ -51,6 +55,7 @@ def VerifyResult(list):
 		return "Unpassed"
 
 
+
 def ExtractAnswer(list):
 	result = []
 	for i in list[0]:
@@ -58,6 +63,8 @@ def ExtractAnswer(list):
 	
 	return result[1:]
 		
+
+
 def SplitToBlock(total):
 	total.append('\n')
 	split_list = []
@@ -75,22 +82,24 @@ def SplitToBlock(total):
 	return split_list
 
 
-def Proc(Name, Type, Dst, DstPort, KeyPath, DetailDebug=0, ShortDebug=0):
+
+#	发送一次dig查询，并且返回该次查询的答案以及对答案校验的结果
+def Proc(Name, Type, Dst, DstPort, KeyPath, DetailDebug=1, ShortDebug=0):
 	cmd = "dig @%s %s %s -p %s +sigchase +trusted-key=%s" % (Dst,Name,Type,DstPort,KeyPath)
 	if DetailDebug:
-		print "|%s debug info start %s|" % ('-'*15,'-'*15)
+#		print "|%s debug info start %s|" % ('-'*15,'-'*15)
 		cmdseq = "%s | cat -n" % cmd
 		getdetail = subprocess.Popen(cmdseq, shell=True, stdout=subprocess.PIPE)
 		getdetail.wait()
-		print getdetail.stdout.read()
-		print "|%s debug info end   %s|" % ('-'*15,'-'*15)
+		logging.debug(getdetail.stdout.read())
+#		print "|%s debug info end   %s|" % ('-'*15,'-'*15)
 	elif ShortDebug:
-		print "|%s debug info start %s|" % ('-'*15,'-'*15)
+#		print "|%s debug info start %s|" % ('-'*15,'-'*15)
 		cmdseq = "drill @%s %s %s -S -k %s -p %s" % (Dst,Name,Type,KeyPath,DstPort)
 		getdetail = subprocess.Popen(cmdseq, shell=True, stdout=subprocess.PIPE)
 		getdetail.wait()
-		print getdetail.stdout.read()
-		print "|%s debug info end   %s|" % ('-'*15,'-'*15)
+		logging.debug(getdetail.stdout.read())
+#		print "|%s debug info end   %s|" % ('-'*15,'-'*15)
 	else:
 		pass
 	sub=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
@@ -99,11 +108,14 @@ def Proc(Name, Type, Dst, DstPort, KeyPath, DetailDebug=0, ShortDebug=0):
 	lBlocks = SplitToBlock(lLineList)
 	sStatus = VerifyResult(lBlocks)
 	lAnswer = ExtractAnswer(lBlocks)
-	pprint.pprint(lAnswer)
+	logging.debug(pprint.pformat(lAnswer))
 	
 	return sStatus,lAnswer
 
 
+
+#	批处理文件中的查询，文件中的每一行都对应3个问题(A,NS,DS),多次调用Proc()函数，
+#	来返回查询结果和校验结果。
 def ProcFile(BatchPath):
 	outputFileName = BatchPath.strip().split('/')[-1].split('.')[-2]+".out"
 	fileIn = open(BatchPath, 'r')
@@ -111,16 +123,46 @@ def ProcFile(BatchPath):
 	lQuery = fileIn.readlines()
 	for eachline in lQuery:
 		item = eachline.strip().split()
-		sStatusNS,lAnswerNS = Proc(item[0],item[1],item[2],item[3],item[4])
+		sStatusNS,lAnswerNS = Proc(item[0],"NS",item[1],item[2],item[3])
 #		print lAnswerNS
 		for k in lAnswerNS:
-			fileOut.write("%s\t%s\t%s\t%s\t%s\n" % (k[0],k[1],k[2],k[3],k[4]))
+#			fileOut.write("%s\t%s\t%s\t%s\t%s\n" % (k[0],k[1],k[2],k[3],k[4]))
+			fileOut.write('\t'.join(k)+'\n')
 		for i in lAnswerNS:
-			sStatusA,lAnswerA = Proc(i[4],'A',item[2],item[3],item[4])
+			sStatusA,lAnswerA = Proc(i[4],'A',item[1],item[2],item[3])
 			for j in lAnswerA:
-				fileOut.write("%s\t%s\t%s\t%s\t%s\n" % (j[0],j[1],j[2],j[3],j[4]))
+#				fileOut.write("%s\t%s\t%s\t%s\t%s\n" % (j[0],j[1],j[2],j[3],j[4]))
+				fileOut.write('\t'.join(j)+'\n')
+	for eachline in lQuery:
+		item = eachline.strip().split()
+		sStatusNS,lAnswerNS = Proc(item[0],"DS",item[1],item[2],item[3])
+		for k in lAnswerNS:
+#			print k
+#			fileOut.write("%s\t%s\t%s\t%s\t%s\n" % (k[0],k[1],k[2],k[3],k[4]))
+			fileOut.write('\t'.join(k)+'\n')
+
+
 	fileIn.close()
 	fileOut.close()
+	return outputFileName
+
+
+#初始化日志模块。
+def init(LogPath):
+	#init logging mode
+	if LogPath != "null":
+		if os.path.exists(LogPath) == False:
+			os.makedirs(LogPath)
+	else:
+		LogPath = '.'
+	
+	logging.basicConfig(level = logging.DEBUG,\
+					format = '%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',\
+					datefmt = '%a, %d %m %Y %H:%M:%S',	\
+					filename = "%s/run.log" % LogPath,	\
+					filemode = 'a')
+	logging.info("############################ A New restart ############################")
+
 
 
 def main(argv):
@@ -130,11 +172,11 @@ def main(argv):
 	Name		= "."
 	Type		= "NS"
 	FilePath	= "null"
-	ShortDebug	= 0
+	ShortDebug	= 1
 	DetailDebug = 0
 	LogPath		= "null"
 	BatchPath	= "null"
-	KeyPath		= "/var/namedFaker/trusted-key.key"
+	KeyPath		= "./trustedkey.key"
 	try:
 		opts,args = getopt.getopt(argv[1:], "hvdDt:p:l:f:k:", ["help"])
 	except getopt.GetoptError,info:
@@ -161,7 +203,7 @@ def main(argv):
 		elif option in ('-f'):
 			BatchPath = value
 		elif option in ('-k'):
-			Keypath = value
+			KeyPath = value
 		else:
 			p_usage()
 			sys.exit()
@@ -175,9 +217,11 @@ def main(argv):
 		if BatchPath == "null":
 			PrintUsage()
 			return 0
+	
+	init(LogPath)
 
 	if BatchPath == "null":
-		Proc(Name, Type, Dst, DstPort, KeyPath, DetailDebug,ShortDebug)
+		Proc(Name, Type, Dst, DstPort, KeyPath, DetailDebug, ShortDebug)
 	else:
 		ProcFile(BatchPath)
 
--- Client/PeroidicCheckRR.py
+++ Client/PeroidicCheckRR.py
@@ -1,47 +1,77 @@
+#coding=utf-8
 #!/usr/bin/env python
 import getopt
 import sys
 import subprocess
-import DebugInfo
-import FindCheck
 import pprint
 import re
 import sched
 import time
-import CheckRR
+import ExchangeClient
+import os
 
-OutputPath = r"/root/Tokangning/ZoneExchange"
-OutputName = r"root.zone.exchange"
-storepathB = r"/home/Tokangning/ZoneExchange.Bak"
-storepathT = r"/home/Tokangning/ZoneExchange"
+DownloadPeriod = 12*3600
+OutputName = "root.zone.exchangedata"
+storepathB = "/var/ZoneExchange/Data/ZoneExchangeBak/"
+storepathT = "/var/ZoneExchange/Data/ZoneExchange/"
+QueryFile = "./Query.in"		
 numcounter = 1
 
-
+#	
 def perform_command(schedule,delay_s):
-	global numcounter
 	schedule.enter(delay_s,0,perform_command,(schedule,delay_s))
 	print "run once"
-	CheckRR.ProcFile("./queryfile.in")
-	cmd1 = "rm %s/*.*" % storepathT
+	OutputFilename = ExchangeClient.ProcFile(QueryFile)
+	TimedOutputFilename = time.strftime("%F-%H:%M-")+OutputFilename
+
+	cmd1 = "rm -f %s*.*" % storepathT
 	sub1 = subprocess.Popen(cmd1,shell=True)
 	sub1.wait()
-	cmd2 = "mv ./queryfile.out %s/%s" % (storepathB,"queryfile.out"+str(numcounter))
+
+	cmd2 = "mv ./%s %s%s" % (OutputFilename, storepathB,TimedOutputFilename)
 	sub2 = subprocess.Popen(cmd2,shell=True)
 	sub2.wait()
-	cmd3 = "cp %s/%s %s/%s" % (storepathB,"queryfile.out"+str(numcounter),storepathT,OutputName)
+
+	cmd3 = "cp %s%s %s%s" % (storepathB, TimedOutputFilename, storepathT, OutputName)
 	sub3 = subprocess.Popen(cmd3,shell=True)
 	sub3.wait()
-	numcounter = numcounter+1
 
-def timing_exe(delay = 60*10):
+def timing_exe(delay = 60):
+	delay = int(DownloadPeriod)
 	schedule = sched.scheduler(time.time,time.sleep)
 	schedule.enter(delay,0,perform_command,(schedule,delay))
 	schedule.run()
+
+def init():
+	
+	global OutputName
+	global storepathB
+	global storepathT
+	global QueryFile
+	global DownloadPeriod
+
+	#init parameter
+	f = open("./Configuration.in")
+	lines = f.readlines()
+	DownloadPeriod = lines[0].strip().split()[-1]
+	OutputName = lines[1].strip().split()[-1]
+	storepathB = lines[2].strip().split()[-1]+lines[3].strip().split()[-1]
+	storepathT = lines[2].strip().split()[-1]+lines[4].strip().split()[-1]
+	QueryFile = lines[5].strip().split()[-1]
+
+	#init folder
+	if os.path.exists(storepathB) == False:
+		os.makedirs(storepathB)
+	if os.path.exists(storepathT) == False:
+		os.makedirs(storepathT)
+
+
 
 
 
 
 if __name__ == "__main__":
+	init()
 	print "start periodic task!"
 	timing_exe()
 
